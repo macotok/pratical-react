5e42e5a3eea7cf0fd0c9026f135abf5b56905643
macotok <monochromer@r8.ucom.ne.jp>
2020-08-22 14:35:41 +0900

    implement react-idle-timer

---
+++ react-idle-timer/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import IdleTimerContainer from './components/IdleTimerContainer';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <IdleTimerContainer />
+    </div>
+  );
+}
+
+export default App;
